1a6434f9ec41eef8daafe3aac03ac61d87c57d97
Jesse Wolgamott <jesse@comalproductions.com>
2016-06-06 10:00:15 -0500

    How many trips did each employee pilot

--- data_parser.rb
+++ data_parser.rb
@@ -16,22 +16,32 @@ all_shipment_revenue = shipments.map do |shipment|
 end
 
 fry_total = 0
+fry_count = 0
+
 bender_total = 0
+bender_count = 0
+
 leela_total = 0
+leela_count = 0
+
 amy_total = 0
+amy_count = 0
 
 shipments.each do |shipment|
   if shipment["Destination"] == "Earth"
     fry_total += shipment["Money"].to_i
+    fry_count += 1
   elsif shipment["Destination"] == "Mars"
     amy_total += shipment["Money"].to_i
+    amy_count += 1
   elsif shipment["Destination"] == "Uranus"
     bender_total += shipment["Money"].to_i
+    bender_count += 1
   else
     leela_total += shipment["Money"].to_i
+    leela_count += 1
   end
 end
-
 
 
 # all_shipment_review => an array of integers
